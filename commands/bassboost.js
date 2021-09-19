b70b339019f6d5ab33832e0518b585071993dd27
betich <betich365@gmail.com>
2021-09-19 22:42:37 +0700

    :tada: fix bassboost and make it work with options

--- commands/bassboost.js
+++ commands/bassboost.js
@@ -3,21 +3,42 @@ const { sendError } = require('../utils/logs');
 module.exports = {
   name: 'bassboost',
   description: 'Toggles bassboost filter',
+  options: [
+    {
+      name: 'enabled',
+      type: 'INTEGER',
+      description: 'Status of the bassboost filter',
+      required: true,
+      choices: [
+        {
+          name: 'Off',
+          value: false,
+        },
+        {
+          name: 'On',
+          value: true,
+        },
+      ],
+    },
+  ],
   async execute(interaction, player, client) {
     await interaction.deferReply();
     const queue = player.getQueue(interaction.guildId);
 
+    const filterEnabled = Boolean(interaction.options.get('enabled').value);
+
     if (!queue || !queue.playing) return void interaction.followUp({ embeds: [sendError('no_current_music', client)] });
 
+    // old method: !queue.getFiltersEnabled().includes('bassboost')
     await queue.setFilters({
-      bassboost: !queue.getFiltersEnabled().includes('bassboost'),
-      normalizer2: !queue.getFiltersEnabled().includes('bassboost'), // because we need to toggle it with bass
+      bassboost: filterEnabled,
+      normalizer2: filterEnabled, // because we need to toggle it with bass
     });
 
     return void interaction.followUp({
       embeds: [
         {
-          descripition: `🎵 | Bassboost ${queue.getFiltersEnabled().includes('bassboost') ? 'Enabled' : 'Disabled'}!`,
+          description: `🎵 | Bassboost ${queue.getFiltersEnabled().includes('bassboost') ? 'Enabled' : 'Disabled'}!`,
           color: client.config.color,
         },
       ],
