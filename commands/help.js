e3e1f72597d73dde24b77006de27ef6f9cc4d72c
betich <betich365@gmail.com>
2021-09-22 04:13:11 +0700

    :tada: add basic help command style

--- commands/help.js
+++ commands/help.js
@@ -4,17 +4,30 @@ module.exports = {
   name: 'help',
   description: 'List all available commands.',
   execute(interaction, player, client) {
-    let str = '';
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    let fields = [];
+    const allCommands = client.commands;
 
-    for (const file of commandFiles) {
-      const command = require(`./${file}`);
-      str += `Name: ${command.name}, Description: ${command.description} \n`;
+    for (const [, command] of allCommands.entries()) {
+      fields.push({
+        name: `⠀\n${command.name}`,
+        value: `${command.description}`,
+      });
     }
 
     return void interaction.reply({
-      embeds: [{ description: str, color: client.config.color }],
-      ephemeral: true,
+      embeds: [
+        {
+          description:
+            'Please note that Lilac is still very early in development. Please give it some time and consider [contributing](https://github.com/betich/lilac) to us on github. Thank you for your patience <3',
+          color: 0x7734eb,
+        },
+        {
+          title: '🙋‍♀️ Help',
+          description: "use discord's slash (/) commands to run a command",
+          color: client.config.color,
+          fields,
+        },
+      ],
     });
   },
 };
